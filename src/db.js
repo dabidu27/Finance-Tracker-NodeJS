87679be175589f2d8edf146da489fa28cc742826
david <adamita.david@gmail.com>
2026-02-21 15:46:12 +0200

    Created DB connection

--- src/db.js
+++ src/db.js
@@ -1,5 +1,7 @@
 import pg from 'pg';
 import 'dotenv/config';
 
-const { Pool } = pg;
-export const pool = new Pool({ connectionString: process.env.DBURL });
+//pg is the driver for node.js <-> sql communication
+const { Pool } = pg; //this means "look into the pg object, find the Pool property, and pool it into it s own variable" <=> const Pool = pg.Pool
+export const pool = new Pool({ connectionString: process.env.DBURL }); //we instantiate an object of the Pool class
+//a pool is a connection with more workers for speed and crash prevention
