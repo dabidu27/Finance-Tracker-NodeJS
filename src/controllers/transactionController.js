b966323b7684aeec61a7780247f45777f74d2fcc
david <adamita.david@gmail.com>
2026-02-21 16:14:18 +0200

    Added addTransaction function

--- src/controllers/transactionController.js
+++ src/controllers/transactionController.js
@@ -11,3 +11,24 @@ export const getTransactions = async (req, res) => {
         res.status(400).json({ 'message': 'Failed to fetch transactions' });
     }
 }
+
+export const addTransaction = async (req, res) => {
+
+    try {
+
+        const { description, amount, category } = req.body; //destructuring the body into 3 variables
+        //equivalent to:
+        //description = req.body.description, amount = req.body.amount etc.
+
+        const query = 'insert into transactions (description, amount, category) values ($1, $2, $3) returning *';
+        //returning * means the query returns to use the row it just inserted after the insertion is finished, so we can send it back in the response
+        const values = [description, amount, category];
+        const result = await pool.query(query, values);
+        res.status(201).json(result.rows[0]);
+
+    } catch (error) {
+
+        console.error(error);
+        res.status(400).json({ 'message': 'Failed to add transaction' });
+    }
+}
